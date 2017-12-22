9cdcf117ebd973480ccebca40f1bb71e7bc04ffd
Sloan Holzman <sloan.holzman@gmail.com>
2017-12-22 12:19:48 -0500

    refactor create.js file

--- notes-app-api/libs/dynamodb-lib.js
+++ notes-app-api/libs/dynamodb-lib.js
@@ -1,4 +1,6 @@
 import AWS from "aws-sdk";
-AWS.config.update({ region: "us-east-1" }); export function call(action, params) {
-const dynamoDb = new AWS.DynamoDB.DocumentClient(); return dynamoDb[action](params).promise();
+AWS.config.update({ region: "us-east-1" });
+export function call(action, params) {
+  const dynamoDb = new AWS.DynamoDB.DocumentClient();
+  return dynamoDb[action](params).promise();
 }
--- notes-app-api/libs/response-lib.js
+++ notes-app-api/libs/response-lib.js
@@ -6,11 +6,12 @@ export function failure(body) {
   return buildResponse(500, body);
 }
 
-function buildResponse(statusCode, body) { return {
+function buildResponse(statusCode, body) { 
+  return {
      statusCode: statusCode,
      headers: {
        "Access-Control-Allow-Origin": "*",
-"Access-Control-Allow-Credentials": true },
+       "Access-Control-Allow-Credentials": true },
      body: JSON.stringify(body)
    };
 }
