36d7325d1b0ac5df57d264146a1084d8bf92c26f
Sloan Holzman <sloan.holzman@gmail.com>
2017-12-22 14:07:45 -0500

    add update and delete APIs

--- notes-app-api/.webpack/service/delete.js
+++ notes-app-api/.webpack/service/delete.js
@@ -92,26 +92,22 @@ var main = exports.main = function () {
           case 0:
             params = {
               TableName: "notes",
-              // 'KeyConditionExpression' defines the condition for the query
-              // - 'userId = :userId': only return items with matching 'userId'
-              //   partition key
-              // 'ExpressionAttributeValues' defines the value in the condition
-              // - ':userId': defines 'userId' to be Identity Pool identity id
-              //   of the authenticated user
-              KeyConditionExpression: "userId = :userId",
-              ExpressionAttributeValues: {
-                ":userId": event.requestContext.identity.cognitoIdentityId
+              // 'Key' defines the partition key and sort key of the item to be removed
+              // - 'userId': Identity Pool identity id of the authenticated user
+              // - 'noteId': path parameter
+              Key: {
+                userId: event.requestContext.identity.cognitoIdentityId,
+                noteId: event.pathParameters.id
               }
             };
             _context.prev = 1;
             _context.next = 4;
-            return dynamoDbLib.call("query", params);
+            return dynamoDbLib.call("delete", params);
 
           case 4:
             result = _context.sent;
 
-            // Return the matching list of items in response body
-            callback(null, (0, _responseLib.success)(result.Items));
+            callback(null, (0, _responseLib.success)({ status: true }));
             _context.next = 11;
             break;
 
